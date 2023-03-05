bf490c71444d8f4420a5d0be95052f336963906a
ultrx000 <ultr4op@gmail.com>
2023-03-05 21:21:47 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#n = int(input("Введите n: "))
+#sum = q = int = int(input("Введите q: "))
+#d = int(input("Введите d: "))
+#while n-1:
+    #sum += d
+    #n -= 1
+
+#print(sum)
+
+b = int(input("Введите число\t "))
+q = int(input("Введите число\t "))
+n = int(input("Введите число\t"))
+if(q>1):
+    S_n = (b*(q**n))/(q-1)
+else:
+    S_n = (b*(q**n))/(q-1)
+print("Сумма первых n членов\t", S_n)
